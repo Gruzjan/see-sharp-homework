b39cf59392bd60e9d76e2bc1985e4bc0108be790
Gruzjan <gracjan122@protonmail.com>
2022-09-30 00:21:28 +0200

    Add project files.

---
+++ Petrol.Shared/Data.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace Petrol.Shared
+{   
+    public class Data
+    {
+        string lpgFile = Properties.Resources.lpg;
+        public int pbRefillCount = 0;
+        public int lpgRefillCount = 0;
+        public decimal pbStatus = 45;
+        public decimal lpgStatus = 30;
+        public List<DateTime> lpgOnlyDays = new List<DateTime>();
+        public DateTime firstLowOnGasDay;
+        public string json = "[";
+        public decimal lpgLiters = 0;
+        public decimal pbLiters = 0;
+
+        public Data()
+        {
+            string[] allLines = lpgFile.Split("\r\n");
+
+            var distances = from line in allLines.Skip(1)
+                            let elements = line.Split("\t")
+                            select Convert.ToInt32(elements[1]);
+
+            var dates = from line in allLines.Skip(1)
+                        let elements = line.Split("\t")
+                        select Convert.ToDateTime(elements[0]);
+
+            for (int i = 0; i < distances.Count(); i++)
+            {
+                json += string.Format("{{\"Date\":\"{0}\",\"Przed\":{1},\"Po\":", dates.ElementAt(i).ToShortDateString(), lpgStatus.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+
+                if (lpgStatus > 15)
+                {
+                    decimal result = Math.Round((9m * distances.ElementAt(i) / 100m), 2);
+                    lpgLiters += result;
+                    lpgStatus -= result;
+                    lpgOnlyDays.Add(dates.ElementAt(i));
+                }
+                else
+                {
+                    if(lpgStatus < 5.25m && firstLowOnGasDay == DateTime.MinValue)
+                        firstLowOnGasDay = dates.ElementAt(i);
+
+                    decimal result = Math.Round((9m * distances.ElementAt(i) / 2 / 100m), 2);
+                    lpgLiters += result;
+                    lpgStatus -= result;
+
+                    result = Math.Round((6m * distances.ElementAt(i) / 2 / 100m), 2);
+                    pbLiters += result;
+                    pbStatus -= result;
+                }
+
+                if (lpgStatus < 5)
+                {
+                    lpgRefillCount++;
+                    lpgStatus = 30;
+                }
+
+                if(pbStatus < 40 && dates.ElementAt(i).DayOfWeek == DayOfWeek.Thursday)
+                {
+                    pbRefillCount++;
+                    pbStatus = 45;
+                }
+
+                if (i == distances.Count() - 1)
+                    json += string.Format("{0}}}]", lpgStatus.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+                else
+                    json += string.Format("{0}}},", lpgStatus.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+            }
+        }
+
+        public void saveToJson()
+        {
+            File.WriteAllText("./data.json", json);
+        }
+
+    }
+}
---
+++ Petrol/Program.cs
@@ -0,0 +1,34 @@
+﻿using Petrol.Shared;
+using System.Linq.Expressions;
+
+public class Program
+{
+    public static void Main()
+    {
+        Data info = new Data();
+
+        // 1. zadanie
+        //Console.WriteLine($"PB95: {info.pbRefillCount} razy, a LPG: {info.lpgRefillCount} razy.");
+
+
+        // 2. zadanie
+        /*foreach (var elem in info.lpgOnlyDays)
+        {
+            Console.WriteLine(elem);
+        }*/
+
+
+        // 3. zadanie
+        //Console.WriteLine(info.firstLowOnGasDay);
+
+
+        //4. zadanie
+        //Console.WriteLine(info.json);
+        //info.saveToJson();
+
+
+        // 5. zadanie
+        Console.WriteLine($"Koszt LPG: {Math.Round(info.lpgLiters * 2.29m, 2)} + 1600 = {Math.Round(info.lpgLiters * 2.29m + 1600, 2)}\nKoszt PB95: {Math.Round(info.pbLiters * 4.99m, 2)}");
+
+    }
+}
